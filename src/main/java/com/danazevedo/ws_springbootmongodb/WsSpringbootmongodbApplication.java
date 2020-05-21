405407234a349a041408eebdab1342b597e82a40
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-05-21 13:31:32 -0300

    fs commit

---
+++ src/main/java/com/danazevedo/ws_springbootmongodb/WsSpringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.danazevedo.ws_springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringbootmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danazevedo/ws_springbootmongodb/WsSpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danazevedo.ws_springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
